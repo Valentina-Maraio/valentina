af78a940f2b919379930badcc408c271864190e7
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-26 12:51:01 +0100

    new font

--- src/components/valentina/Valentina.js
+++ src/components/valentina/Valentina.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Valentina.css";
+import '../../App.css'
 import { Container, Row, Col } from "react-bootstrap";
 import ciao from "../../logo.svg";
 
@@ -9,9 +10,9 @@ const Valentina = () => {
       <Container>
         <Row>
           <Col className="uno" xs={12} md={8}>
-            <div class="valentina">
-                <h2>Valentina Maraio</h2>
-                <h3>Web developer</h3>
+            <div className="valentina font-face-gm" >
+                <h1>VALENTINA MARAIO</h1>
+                <h2>Web developer</h2>
                 <p>qualcosa che ancora non so anche se ciao ho freddo</p>
                 <p>ciao come va? ho fame! forse devo bere un po di acqua</p>
                 <p>oggi mangio curry con tofuuuuu. Gracias señora!</p>
